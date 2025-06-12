1f8624a63664861f03533d8d131a1fddf2db715e
John Alexander Ortiz <johnspartanunsc@gmail.com>
2025-06-12 10:35:23 -0500

    HealthBar

--- Assets/Scripts/HealthPlayer.cs
+++ Assets/Scripts/HealthPlayer.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class HealthPlayer : MonoBehaviour
 {
 
     private float health;
+    private float healthMaxime = 100;
     private TextMeshProUGUI textMesh;
+    private Image rellenoBarraVida;
 
     void Start()
     {
         health = 100f;
-        textMesh = GetComponent<TextMeshProUGUI>();
+        //textMesh = GetComponent<TextMeshProUGUI>();
+        rellenoBarraVida = GameObject.FindGameObjectWithTag("FillHealth").GetComponent<Image>();
     }
     private void Update()
     {
-        textMesh.text = health.ToString("0");
+        //textMesh.text = health.ToString("0");
+        rellenoBarraVida.fillAmount = health / healthMaxime;
     }
 
     public void HealthChange(float Number)
