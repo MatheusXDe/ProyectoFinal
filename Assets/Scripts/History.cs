21e0f548b9e1e664e2b24984fb7c80ecebe3076b
Leidy <leidy02karib@gmail.com>
2025-06-13 15:58:42 -0500

    Detalles Interfaz
    -Panel de controles (el que debe aparece antes del tutorial)
    -Audio typing
    -Detalles decoración panel history
    -Creditos

--- Assets/Scripts/History.cs
+++ Assets/Scripts/History.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 
-public class Dialogue : MonoBehaviour
+public class History : MonoBehaviour
 {
     [SerializeField] private GameObject historyPanel;
     [SerializeField] private TMP_Text historyText;
+    [SerializeField] private GameObject acceptButton;
+    [SerializeField] private AudioClip typeSound;
+    private AudioSource audioSource;
+    private float lastSoundTime;
     [SerializeField, TextArea(4, 6)] private string[] historyLines;
 
 
@@ -16,8 +20,9 @@ public class Dialogue : MonoBehaviour
 
     void OnEnable()
     {
-        // Se ejecuta cuando el panel History se activa
         StartHistory();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.ignoreListenerPause = true;
     }
 
     void Update()
@@ -44,6 +49,7 @@ public class Dialogue : MonoBehaviour
         historyPanel.SetActive(true);
         lineIndex = 0;
         Time.timeScale = 0f;
+        acceptButton.SetActive(false);
         StartCoroutine(ShowLine());
     }
 
@@ -56,24 +62,43 @@ public class Dialogue : MonoBehaviour
         }
         else
         {
-            didHistoryStart = false;
-            historyPanel.SetActive(false);
-            Time.timeScale = 1f;
+
+            isTyping = false;
+            acceptButton.SetActive(true);
+  
         }
     }
 
     private IEnumerator ShowLine()
     {
+        int charCounter = 0;
         isTyping = true;
         historyText.text = string.Empty;
+        lastSoundTime = 0f;
+
 
         foreach (char ch in historyLines[lineIndex])
         {
             historyText.text += ch;
+            charCounter++;
+
+            if (typeSound != null && Time.unscaledTime - lastSoundTime > 0.16f)
+            {
+                audioSource.PlayOneShot(typeSound);
+                lastSoundTime = Time.unscaledTime;
+            }
+
             yield return new WaitForSecondsRealtime(typingTime);
         }
-
         isTyping = false;
+
+    }
+
+    public void CloseHistory()
+    {
+        didHistoryStart = false;
+        historyPanel.SetActive(false);
+        Time.timeScale = 1f;
     }
 
 
